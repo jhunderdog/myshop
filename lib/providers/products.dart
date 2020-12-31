e9a71b8e57ffeaa1fc0f5b049a818bb98806259e
jhunderdog <33382water@gmail.com>
2020-12-31 19:52:54 +0900

    Delete

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:myshop/models/http_exception.dart';
 import './product.dart';
 
 class Products with ChangeNotifier {
@@ -138,9 +139,20 @@ class Products with ChangeNotifier {
     }
   }
 
-  void deleteProduct(String id) {
-    _items.removeWhere((prod) => prod.id == id);
+  Future<void> deleteProduct(String id) async {
+    final url =
+        'https://flutter-update-fea3d-default-rtdb.firebaseio.com/products/$id.json';
+    final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
+    var existingProduct = _items[existingProductIndex];
+    _items.removeAt(existingProductIndex);
     notifyListeners();
+    final response = await http.delete(url);
+    if (response.statusCode >= 400) {
+      _items.insert(existingProductIndex, existingProduct);
+      notifyListeners();
+      throw HttpException('Colud not delete product.');
+    }
+    existingProduct = null;
   }
 
   Product findById(String id) {
