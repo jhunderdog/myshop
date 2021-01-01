2d5f6e217a056d1a387c73ffe2d690e25c3fbfba
jhunderdog <33382water@gmail.com>
2021-01-01 11:00:01 +0900

    Fetch Orders

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -70,6 +70,9 @@ class Products with ChangeNotifier {
       print(json.decode(response.body));
       final extractedData = json.decode(response.body) as Map<String, dynamic>;
       final List<Product> loadedProducts = [];
+      if (extractedData == null) {
+        return;
+      }
       extractedData.forEach((prodId, prodData) {
         loadedProducts.add(Product(
           id: prodId,
