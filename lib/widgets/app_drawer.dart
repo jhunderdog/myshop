478a6fee6baf9171b86c96dee3de1a4418a41beb
jhunderdog <33382water@gmail.com>
2021-01-01 20:06:06 +0900

    Auto Logout with Timer

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:myshop/screens/orders_screen.dart';
 import 'package:myshop/screens/user_product_screen.dart';
+import 'package:provider/provider.dart';
+import '../providers/auth.dart';
 
 class AppDrawer extends StatelessWidget {
   @override
@@ -34,6 +36,15 @@ class AppDrawer extends StatelessWidget {
             onTap: () {
               Navigator.of(context)
                   .pushReplacementNamed(UserProductScreen.routeName);
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Logout'),
+            onTap: () {
+              Navigator.of(context).pop(); // close the drawer
+              Provider.of<Auth>(context, listen: false).logout();
             },
           ),
         ],
