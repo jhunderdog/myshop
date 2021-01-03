dd56146389e7e76d185a10f6fe6d2d79c1f66922
jhunderdog <33382water@gmail.com>
2021-01-03 12:10:26 +0900

    AnimatedContainer with expanded

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -39,39 +39,34 @@ class _OrderItemState extends State<OrderItem> {
             ),
             // if (_expanded)
             AnimatedContainer(
-              duration: Duration(milliseconds: 300),
-              child: Container(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 15,
-                    vertical: 4,
-                  ),
-                  height: _expanded
-                      ? min(widget.order.products.length * 20.0 + 10, 100)
-                      : 0,
-                  child: ListView(
-                    children: widget.order.products
-                        .map((prod) => Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  prod.title,
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                  ),
+                duration: Duration(milliseconds: 300),
+                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+                height: _expanded
+                    ? min(widget.order.products.length * 20.0 + 10, 100)
+                    : 0,
+                child: ListView(
+                  children: widget.order.products
+                      .map((prod) => Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                prod.title,
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
                                 ),
-                                Text(
-                                  '${prod.quantity}x \$${prod.price}',
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    color: Colors.grey,
-                                  ),
+                              ),
+                              Text(
+                                '${prod.quantity}x \$${prod.price}',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.grey,
                                 ),
-                              ],
-                            ))
-                        .toList(),
-                  )),
-            )
+                              ),
+                            ],
+                          ))
+                      .toList(),
+                )),
           ],
         ),
       ),
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -33,7 +33,7 @@ class ProductItem extends StatelessWidget {
           child: Hero(
             tag: product.id,
             child: FadeInImage(
-              placeholder: AssetImage('/assets/images/original.png'),
+              placeholder: AssetImage('assets/images/original.png'),
               image: NetworkImage(product.imageUrl),
               fit: BoxFit.cover,
             ),
